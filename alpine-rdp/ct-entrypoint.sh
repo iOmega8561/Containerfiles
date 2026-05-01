a5b84146319d146a278a55e71c0e3cd32e4bfb19
iOmega8561 <giusepperocco38@gmail.com>
2026-05-01 15:26:22 +0200

    Removed multi-stage "flavoured" build ct-entrypoint has been updated, now sets locales according to existing environment

--- alpine-rdp/ct-entrypoint.sh
+++ alpine-rdp/ct-entrypoint.sh
@@ -6,6 +6,14 @@ then
     exit 1
 fi
 
+lc_all=${LC_ALL:-C.UTF-8}
+lang=${LANG:-C.UTF-8}
+
+cat <<EOF > /etc/profile.d/locale.sh
+export LANG="${lang}"
+export LC_ALL="${lc_all}"
+EOF
+
 echo -e "$PASSWD\n$PASSWD" | sudo passwd default > /dev/null 2>&1
 echo "Password set to $PASSWD"
 
